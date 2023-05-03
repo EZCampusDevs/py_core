95fb768f996fef3b6bfbc36af4260644cc918461
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2023-05-03 11:25:52 -0400

    Added validator for Faculty

--- classes/faculty_class.py
+++ classes/faculty_class.py
@@ -5,7 +5,7 @@ The Faculty class and by extension the module's functions represent universal fa
 
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Faculty(BaseModel):
@@ -21,3 +21,11 @@ class Faculty(BaseModel):
     name: Optional[str]
     email: Optional[str]
     rating: Optional[int]
+
+    @validator("rating")
+    def verify_valid_rating(cls, v):
+        if v is not None and not isinstance(v, int):
+            raise ValueError(f"Expected type <None> or <int>, got {type(v)}")
+        if isinstance(v, int) and not (0 <= v <= 100):
+            raise ValueError(f"With <int> expected 0 <= rating <= 100, got rating={v}")
+        return v
