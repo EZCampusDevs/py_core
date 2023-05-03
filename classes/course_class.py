7a6e13955cddfa6fbc1f4a008cbc2d15b6993a74
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2023-05-03 13:17:47 -0400

    Added new parameters for Course class

--- classes/course_class.py
+++ classes/course_class.py
@@ -5,120 +5,49 @@ The Course class and by extension the module's functions represent universal cou
 
 from typing import Optional, List
 
-from pydantic import BaseModel, root_validator
+from pydantic import BaseModel, validator
 
 from .extended_meeting_class import ExtendedMeeting
+from .faculty_class import Faculty
 from .meeting_class import Meeting, meetings_are_time_valid
 
 
 class Course(BaseModel):
-    """Course class defines a single general course identified by a CRN (course registration number).
-
-    fac: Faculty ID identifying faculty department. (Example: "MATH").
-    uid: (UID = University ID). ID code ending with 'U'. (Example: "1020U").
-    crn: (CRN = Course Registration Number). A unique int that represents each course. (Example: 12345).
-    class_type: Identifies the type of class. (Example: "Lecture", "Tutorial" & "Laboratory").
-    title: Title of the class. (Example: "Calculus II").
-    section: Section identifier, usually a number with possible leading zeros. (Example: "001").
-    class_time: List of Meeting objects. (meeting_class.py).
-    is_linked: Defines if the class has any linked classes that are required.
-    link_tag: Identifies the link type for its c. (Example: "A1").
-        For computation, links are made with matching tags from classes with the same fac and uid. Class with a
-        link_tag="A1" needs to link with another class of link_tag="B#", where # is an integer.
-    seats_filled: Number of seats filled.
-    max_capacity: Maximum capacity of a course.
-    instructor: Course's instructor's name.
-    instructor_email: Course's instructor's email.
-    instructor_rating: Course's instructor's rating.
-    is_virtual: Defines if the class is completely virtual/online.
-    is_restricted: Defines if the class has any restrictions.
-    restrictions: Restrictions in a string format.
-
-    Examples:
-        >>> from datetime import date, time
-        >>> Course(fac="MATH", \
-                uid="1234U", \
-                crn=12345, \
-                class_type="Lecture", \
-                title="Calculus 9000", \
-                section="003", \
-                class_time=[ \
-                    Meeting( \
-                        time_start=time(9, 40), \
-                        time_end=time(11, 0), \
-                        weekday_int=0, \
-                        date_start=date(2022, 1, 17), \
-                        date_end=date(2022, 4, 14), \
-                        repeat_timedelta_days=7, \
-                        location="UOW SYN SYN" \
-                    ), \
-                    Meeting( \
-                        time_start=time(9, 40), \
-                        time_end=time(11, 0), \
-                        weekday_int=3, \
-                        date_start=date(2022, 1, 17), \
-                        date_end=date(2022, 4, 14), \
-                        repeat_timedelta_days=7, \
-                        location="UON SPY SPY9999", \
-                    ) \
-                ], \
-                is_linked=True, \
-                link_tag="A1", \
-                seats_filled=6, \
-                max_capacity=100, \
-                instructor="Last, First", \
-                instructor_email="First.Last@ontariotechu.ca", \
-                instructor_rating=0.84, \
-                is_virtual=False, \
-                restrictions={ \
-                    'Not all restrictions are applicable.': [], \
-                    'Must be enrolled in one of the following Levels: ': \
-                        ['Undergraduate (UG)'], \
-                    'Must be enrolled in one of the following Colleges: ': \
-                        ['Engineering & Applied Science (50)'], \
-                    'Must be enrolled in one of the following Classes: ': \
-                        ['First year (Y1)'], \
-                    'Cannot be enrolled in one of the following Majors: ': \
-                        [ \
-                            'Cool Science (COOL)', \
-                            'Integrative Awesomescience (ASCI)' \
-                        ] \
-                })
-        Course(fac='MATH', uid='1234U', crn=12345, class_type='Lecture', title='Calculus 9000', section='003', class_time=[Meeting(time_start=datetime.time(9, 40), time_end=datetime.time(11, 0), weekday_int=0, date_start=datetime.date(2022, 1, 17), date_end=datetime.date(2022, 4, 14), repeat_timedelta_days=7, location='UOW SYN SYN'), Meeting(time_start=datetime.time(9, 40), time_end=datetime.time(11, 0), weekday_int=3, date_start=datetime.date(2022, 1, 17), date_end=datetime.date(2022, 4, 14), repeat_timedelta_days=7, location='UON SPY SPY9999')], is_linked=True, link_tag='A1', seats_filled=6, max_capacity=100, instructor='Last, First', instructor_email='First.Last@ontariotechu.ca', instructor_rating=0.84, is_virtual=False, restrictions={'Not all restrictions are applicable.': [], 'Must be enrolled in one of the following Levels: ': ['Undergraduate (UG)'], 'Must be enrolled in one of the following Colleges: ': ['Engineering & Applied Science (50)'], 'Must be enrolled in one of the following Classes: ': ['First year (Y1)'], 'Cannot be enrolled in one of the following Majors: ': ['Cool Science (COOL)', 'Integrative Awesomescience (ASCI)']})
-    """
-    fac: str
-    uid: str
-    crn: int
-    class_type: str
-    title: str
-    section: Optional[str]
-    class_time: List[Meeting] = []
-    is_linked: bool
-    link_tag: Optional[str]
-    seats_filled: int
-    max_capacity: int
-    instructor: Optional[str]
-    instructor_email: Optional[str]
-    instructor_rating: Optional[float]
+    """Course class defines a single general course identified by a CRN (course registration number)."""
+    course_code: str  # Aka course code, example: "BIOL1020U".
+    subject: Optional[str]  # Aka subject, example: "BIOL".
+    subject_long: Optional[str]  # Aka subject long, example: "Biology".
+    crn: int  # Aka course reference/registration number: 12345.
+    class_type: str  # Aka class type, example: "Lecture".
+    title: str  # Aka course title, example: "Biology II".
+    section: Optional[str]  # Aka sequence number, example: "001".
+    class_time: List[Meeting] = []  # Class times represented as a list of Meetings.
+    is_linked: bool  # Aka is section linked.
+    link_tag: Optional[str]  # Aka link identifier, example: "A1".
+    seats_filled: int  # Aka enrollment.
+    max_capacity: int  # Aka maximum enrollment.
+    seats_available: Optional[int]  # Defaults if not provided. Aka enrollment seats available.
     is_virtual: bool
-    restrictions: Optional[dict]
+    restrictions: Optional[dict]  # JSON data for class registration restrictions.
+    campus_description: Optional[str]  # Non-essential (api forwarded) data.
+    instructional_method: Optional[str]  # Non-essential (api forwarded) data.
+    instructional_method_description: Optional[str]  # Non-essential (api forwarded) data.
+    is_open: Optional[bool]  # Non-essential (api forwarded) data.  # TODO(Daniel): what is this exactly?
+    wait_count: Optional[int]  # Aka wait-list count. Non-essential (api forwarded) data.
+    wait_max_capacity: Optional[int]  # Aka wait-list capacity. Non-essential (api forwarded) data.
+    wait_available: Optional[int]  # Aka wait-list available. Non-essential (api forwarded) data.
+    credit_hours: Optional[int]  # Non-essential (api forwarded) data.
+    credit_hour_low: Optional[int]  # Non-essential (api forwarded) data.
+    credit_hour_high: Optional[int]  # Non-essential (api forwarded) data.
+    instructors: List[Faculty] = []  # Aka list of Faculty.
 
-    @root_validator()
-    def verify_valid_instructor_rating(cls, values):
-        rt = values.get("instructor_rating")
-        if rt is not None and not isinstance(rt, float):
-            raise ValueError(f"Expected type <None> or <float>, got {type(rt)}")
-        if isinstance(rt, float) and not (0 <= rt <= 1):
-            raise ValueError(f"With <float> expected 0 <= instructor_rating <= 1, got instructor_rating={rt}")
-        return values
-
-    def course_code(self) -> str:
-        """Get the general course code of the Course object.
-
-        Returns:
-            Course code.
-        """
-        return f"{self.fac}{self.uid}"
+    @validator("seats_available", always=True)
+    def confirm_seats_available(cls, v, values):
+        if not isinstance(v, int):
+            sf = values.get("seats_filled")
+            mc = values.get("max_capacity")
+            return mc - sf if mc - sf >= 0 else 0
+        return v
 
     def get_comp_key(self) -> str:
         """Get representative value of course and type used for computation.
@@ -129,11 +58,11 @@ class Course(BaseModel):
         Notes:
             Used in special computations such as schedule optimization and event planning.
         """
-        representation = f"{self.course_code()} {self.class_type}"
+        representation = f"{self.course_code} {self.class_type}"
         # Alternative representation:
-        # representation = (f"{course.course_code()} {course.link_tag[0]}"
+        # representation = (f"{course.course_code} {course.link_tag[0]}"
         #                   if course.link_tag is not None
-        #                   else f"{course.course_code()} None")
+        #                   else f"{course.course_code} None")
         return representation
 
     def num_actual_meetings(self) -> int:
@@ -151,31 +80,17 @@ class Course(BaseModel):
             count += meeting.num_actual_meetings()
         return count
 
-    def get_raw_str(self):
-        """For prototyping purposes only.
-
-        Returns:
-            Default str similar to regular __str__ methods.
-        """
-        return (f"fac={self.fac}\n"
-                f"uid={self.uid}\n"
-                f"crn={self.crn}\n"
-                f"class_type={self.class_type}\n"
-                f"title={self.title}\n"
-                f"section={self.section}\n"
-                f"class_time={self.class_time}\n"
-                f"is_linked={self.is_linked}\n"
-                f"link_tag={self.link_tag}\n"
-                f"seats_filled={self.seats_filled}\n"
-                f"max_capacity={self.max_capacity}\n"
-                f"instructor={self.instructor}\n"
-                f"instructor_email={self.instructor_email}\n"
-                f"instructor_rating={self.instructor_rating}\n"
-                f"is_virtual={self.is_virtual}\n"
-                f"restrictions={self.restrictions}")
-
-    def __str__(self):
-        return self.get_raw_str()
+    def faculty_instructors_text(self) -> str:
+        return ", ".join(
+            [" ".join(
+                [
+                    i.name,  # We always assume the instructor has name filled, else use the code below.
+                    # i.name if isinstance(i.name, str) else '',
+                    f"({i.email})" if isinstance(i.email, str) else '',
+                    f"{f'{i.rating}/100' if isinstance(i.rating, int) else ''}"
+                ]
+            ) for i in self.instructors]
+        ) if self.instructors else "N/A"
 
 
 def schedule_is_time_valid(course_list: list[Course]) -> bool:
@@ -253,16 +168,14 @@ def schedule_to_simplified_json(course_list: list[Course]) -> list[dict]:
     for c in course_list:
         new_event = {
             "title":
-                f"{c.title} {c.class_type[:3].upper()} ({c.course_code()})",
+                f"{c.title} {c.class_type[:3].upper()} ({c.course_code})",
             "description":
-                f"Instructor: {c.instructor}\n"
-                f"Instructor email: {c.instructor_email}\n"
-                f"Instructor rating: {c.instructor_rating}\n"
+                f"Instructor{'s' if len(c.instructors) > 1 else ''}: {c.faculty_instructors_text()}\n"
                 f"CRN: {c.crn}\n"
                 f"Section: {c.section}\n"
                 f"Seats filled: {c.seats_filled}\n"
                 f"Max capacity: {c.max_capacity}\n"
-                f"Registration restrictions: {c.restrictions}",
+                f"Seats available: {c.seats_available}\n",
             "meetings": [
                 {
                     "time_start": mt.time_start.isoformat(),
@@ -290,18 +203,6 @@ def course_to_extended_meetings(course_list: list[Course]) -> list[ExtendedMeeti
     Returns:
         List of translated ExtendedMeeting objects.
     """
-
-    def instructor_details(course: Course) -> str:
-        if isinstance(course.instructor, str):
-            if isinstance(course.instructor_rating, float):
-                return f"{course.instructor} ({course.instructor_email}) {round(c.instructor_rating * 100)}%"
-                # TODO(Daniel): Warning! The above code assumes we will have the instructor email if we have the
-                #  instructor name.
-            else:
-                return f"{course.instructor} ({course.instructor_email})"
-        else:  # course.instructor is None.
-            return "Unspecified"
-
     ex_mt_list = []
     for c in course_list:
         ex_mt_list += [
@@ -313,11 +214,14 @@ def course_to_extended_meetings(course_list: list[Course]) -> list[ExtendedMeeti
                 date_end=mt.date_end,
                 repeat_timedelta_days=mt.repeat_timedelta_days,
                 location="VIRTUAL" if c.is_virtual else str(mt.location),
-                name=f"{c.title} {c.class_type[:3].upper()} ({c.course_code()})",
+                name=f"{c.title} {c.class_type[:3].upper()} ({c.course_code})",
                 description=(
-                    f"Instructor: {instructor_details(course=c)}\n"
+                    f"Instructor{'s' if len(c.instructors) > 1 else ''}: {c.faculty_instructors_text()}\n"
                     f"CRN: {c.crn}\n"
                     f"Section: {c.section}\n"
+                    f"Seats filled: {c.seats_filled}\n"
+                    f"Max capacity: {c.max_capacity}\n"
+                    f"Seats available: {c.seats_available}\n"
                     f"Has linked classes: {c.is_linked}"
                 ),
                 seats_filled=c.seats_filled,
