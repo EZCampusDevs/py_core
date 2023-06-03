bcd170106e17b11a8568ca3a869d23365d8902a0
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2023-06-03 13:07:32 -0400

    Improvements to db code and new get_courses_via_course_id

--- db/course.py
+++ db/course.py
@@ -4,14 +4,12 @@ import os
 from datetime import datetime
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy import event
+from sqlalchemy import create_engine, event, Column, Integer, Boolean, VARCHAR, ForeignKey, Date, UniqueConstraint
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from ..course_tables import (TBL_Course, TBL_Course_Faculty, TBL_Meeting, TBL_Course_Data, TBL_Faculty, TBL_Class_Type)
-from ...classes.course_class import Course, merge_course_meeting_occurrences
-from ...classes.instructor_class import Instructor
-from ...classes.meeting_class import Meeting
+from ..classes.course_class import Course, merge_course_meeting_occurrences
+from ..classes.instructor_class import Instructor
+from ..classes.meeting_class import Meeting
 
 load_dotenv()
 
@@ -40,8 +38,113 @@ def init_database(use_mysql: bool = True, db_host: str = os.getenv("db_host"), d
     Base.metadata.bind = Engine
 
 
+init_database()  # TODO: Fix this to be prod ready, for quick testing purposes only!!!
+
+
+class TBL_Term(Base):
+    __tablename__ = "tbl_term"
+
+    term_id = Column(Integer, primary_key=True)
+    term_description = Column(VARCHAR(128))
+
+
+class TBL_Course(Base):
+    __tablename__ = "tbl_course"
+
+    course_id = Column(Integer, primary_key=True, autoincrement=True)
+    term_id = Column(Integer, ForeignKey("tbl_term.term_id"))
+    course_code = Column(VARCHAR(32))
+    course_description = Column(VARCHAR(128))
+
+    __table_args__ = (UniqueConstraint("term_id", "course_code", name="_term_id_course_code_constraint"),)
+
+
+class TBL_Class_Type(Base):
+    __tablename__ = "tbl_classtype"
+
+    class_type_id = Column(Integer, primary_key=True, autoincrement=True)
+    class_type = Column(VARCHAR(128))
+
+
+class TBL_Course_Data(Base):
+    __tablename__ = "tbl_course_data"
+
+    course_data_id = Column(Integer, autoincrement=True, primary_key=True)
+    course_id = Column(Integer, ForeignKey("tbl_course.course_id"))
+    crn = Column(VARCHAR(32))
+    sequence_number = Column(VARCHAR(128))
+    id = Column(Integer)
+    course_title = Column(VARCHAR(128))
+    subject = Column(VARCHAR(128))
+    subject_long = Column(VARCHAR(128))
+    campus_description = Column(VARCHAR(128))
+    class_type_id = Column(Integer, ForeignKey("tbl_classtype.class_type_id"))
+    open_section = Column(Boolean)
+    maximum_enrollment = Column(Integer)
+    enrollment = Column(Integer)
+    seats_available = Column(Integer)
+    wait_capacity = Column(Integer)
+    wait_count = Column(Integer)
+    credit_hour_high = Column(Integer)
+    credit_hour_low = Column(Integer)
+    link_identifier = Column(VARCHAR(128))
+    is_section_linked = Column(Boolean)
+    instructional_method = Column(VARCHAR(128))
+    instructional_method_description = Column(VARCHAR(128))
+
+    __table_args__ = (UniqueConstraint("course_id", "crn", name="_course_id_crn_constraint"),)
+
+
+class TBL_Course_Faculty(Base):
+    __tablename__ = "tbl_course_faculty"
+
+    course_data_id = Column(Integer, ForeignKey("tbl_course_data.course_data_id"), primary_key=True)
+    faculty_id = Column(Integer, ForeignKey("tbl_faculty.faculty_id"), primary_key=True)
+
+
+class TBL_Faculty(Base):
+    __tablename__ = "tbl_faculty"
+
+    faculty_id = Column(Integer, primary_key=True, autoincrement=True)
+    instructor_name = Column(VARCHAR(128))
+    instructor_email = Column(VARCHAR(128))
+    instructor_rating = Column(Integer)
+
+
+class TBL_Meeting(Base):
+    __tablename__ = "tbl_meeting"
+
+    meeting_id = Column(Integer, autoincrement=True, primary_key=True)
+    course_data_id = Column(Integer, ForeignKey("tbl_course_data.course_data_id"))
+    term_id = Column(Integer, ForeignKey("tbl_term.term_id"))
+    crn = Column(VARCHAR(32))
+    start_date = Column(Date)
+    end_date = Column(Date)
+    begin_time = Column(VARCHAR(128))
+    end_time = Column(VARCHAR(128))
+    days_of_week = Column(Integer)
+    campus = Column(VARCHAR(128))
+    campus_description = Column(VARCHAR(128))
+    building = Column(VARCHAR(128))
+    building_description = Column(VARCHAR(128))
+    room = Column(VARCHAR(128))
+    meeting_schedule_type = Column(VARCHAR(128))
+
+
+def get_courses_via_course_id(course_id_list: list[int]) -> list[Course]:
+    if not course_id_list:
+        return []
+
+    ids = []
+    with Session.begin() as session:
+        ids = session.query(TBL_Course_Data.course_data_id).filter(TBL_Course_Data.course_id.in_(course_id_list)).all()
+        ids = [int(i[0]) for i in ids]
+    return get_courses_via_data_id(data_id_list=ids)
+
+
 def get_courses_via_data_id(data_id_list: list[int]) -> list[Course]:
-    init_database()  # TODO: Fix this to be prod ready, for quick testing purposes only!!!
+    if not data_id_list:
+        return []
 
     course_list = []
     with Session.begin() as session:
@@ -52,8 +155,8 @@ def get_courses_via_data_id(data_id_list: list[int]) -> list[Course]:
             for mt_r in mt_result:
                 meeting_list.append(
                     Meeting(
-                        time_start=datetime.strptime(mt_r.begin_time, "%H%M").time(),
-                        time_end=datetime.strptime(mt_r.end_time, "%H%M").time(),
+                        time_start=datetime.strptime(str(mt_r.begin_time), "%H%M").time(),
+                        time_end=datetime.strptime(str(mt_r.end_time), "%H%M").time(),
                         date_start=mt_r.start_date,
                         date_end=mt_r.end_date,
                         occurrence_unit=None,  # TODO: Temporary hardcode, needs to be calculated at scraper level.
